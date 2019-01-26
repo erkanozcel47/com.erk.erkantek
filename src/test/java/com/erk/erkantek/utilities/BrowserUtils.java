d646846d1ef3d880e67f3a7a13cf421c1ae6a93d
erkanozcel47 <erkanozcelik507@gmail.com>
2019-01-26 15:27:52 -0500

    Browser utils hover over method has been created

--- src/test/java/com/erk/erkantek/utilities/BrowserUtils.java
+++ src/test/java/com/erk/erkantek/utilities/BrowserUtils.java
@@ -1,5 +1,8 @@
 package com.erk.erkantek.utilities;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 public class BrowserUtils {
 	
 	/*
@@ -19,6 +22,17 @@ public class BrowserUtils {
 		Driver.getDriver().switchTo().window(origin);
 	}
 	
+	
+	/*
+	 * This method is hover over an element. 
+	 * it uses actions class and after that uses movetoelement() method 
+	 * basically the end perform the action.
+	 * */
+	
+	public static void hover(WebElement element) {
+		Actions action = new Actions(Driver.getDriver());
+		action.moveToElement(element).perform();
+	}
 	
 
 }
