5ee97863cf1e4831449e7559775bcd4ba111f1fc
erkanozcel47 <erkanozcelik507@gmail.com>
2019-01-26 17:15:20 -0500

    browser utils fluentWait

--- src/test/java/com/erk/erkantek/utilities/BrowserUtils.java
+++ src/test/java/com/erk/erkantek/utilities/BrowserUtils.java
@@ -2,6 +2,9 @@ package com.erk.erkantek.utilities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -10,6 +13,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowserUtils {
@@ -162,6 +166,20 @@ public class BrowserUtils {
 	}
 
 	
+	
+	public static WebElement fluentWait(final WebElement webElement, int timeinsec) {
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(Driver.getDriver())
+				.withTimeout(timeinsec, TimeUnit.SECONDS).pollingEvery(timeinsec, TimeUnit.SECONDS)
+				.ignoring(NoSuchElementException.class);
+		WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				return webElement;
+			}
+		});
+		return element;
+	}
+
+
 	
 	
 	
