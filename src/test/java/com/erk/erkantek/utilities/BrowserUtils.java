d1ea306ef8f5265862a50fa781f9864ed3cbbb35
erkanozcel47 <erkanozcelik507@gmail.com>
2019-01-26 15:21:44 -0500

    Browser util Windows Handler method

--- src/test/java/com/erk/erkantek/utilities/BrowserUtils.java
+++ src/test/java/com/erk/erkantek/utilities/BrowserUtils.java
@@ -1,5 +1,24 @@
 package com.erk.erkantek.utilities;
 
 public class BrowserUtils {
+	
+	/*
+	 * It will take a target title and it can handle an opened window.
+	 * 
+	 * */
+	
+	public static void switchToWindows(String targetTitle) {
+		
+		String origin = Driver.getDriver().getWindowHandle();
+		for (String handle : Driver.getDriver().getWindowHandles()) {
+			Driver.getDriver().switchTo().window(handle);
+			if(Driver.getDriver().getTitle().equals(targetTitle)) {
+				return;
+			}
+		}
+		Driver.getDriver().switchTo().window(origin);
+	}
+	
+	
 
 }
